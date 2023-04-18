fed1c31c5b70448f63847f304fecb76c4531ae07
Tyler Hampton <thampton92@gmail.com>
2023-04-18 16:49:18 -0400

    add hover effect

--- etchascript.js
+++ etchascript.js
@@ -4,4 +4,8 @@ for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
+    
+    square.addEventListener('mouseover', function (e) {
+        e.target.style.background = 'blue';
+    });
 };
