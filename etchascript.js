39768ffc57e0f1dfec0b86ba824ed768732404c5
Tyler Hampton <thampton92@gmail.com>
2023-04-21 15:24:06 -0400

    add resolution button

--- etchascript.js
+++ etchascript.js
@@ -1,11 +1,25 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    let j = prompt('How many squares on a side?')
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    generate(j);
+});
+
+function generate(j) {
+    for (let i = 0; i < (j**2); i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        let k = 500/j;
+        square.style.width = `${k}px`;
+        square.style.height = `${k}px`;
     
-    square.addEventListener('mouseover', function (e) {
-        e.target.style.background = 'blue';
-    });
+        square.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'black';
+        });
+    };
 };
